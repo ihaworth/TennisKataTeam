a5d399cbe48b54a91b8f0da399cd042828b283cc
U-Ian-HP\Ian <Ian@Ian-HP.(none)>
2013-08-30 15:27:59 +0100

    Partial refactoring of the PostDeuceScorer to a state machine based model.  Note the tests fail at the moment as we hit a problem as we ran out of time.

--- src/PostDeuceScorer.java
+++ src/PostDeuceScorer.java
@@ -5,35 +5,113 @@ public class PostDeuceScorer implements Scorer
     private static final String ADVANTAGE_A = "Advantage A";
     private static final String DEUCE = "Deuce";
 
-    private String score = DEUCE;
+
+    public final State deuce = new State(PostDeuceScorer.DEUCE) {
+
+        @Override
+        void aScores(PostDeuceScorer postDeuceScorer)
+        {
+            postDeuceScorer.state = advantageA;
+        }
+
+        @Override
+        void bScores(PostDeuceScorer postDeuceScorer)
+        {
+            postDeuceScorer.state = advantageB;
+        }
+    };
+
+    public final State advantageA = new State(ADVANTAGE_A) {
+
+        @Override
+        void aScores(PostDeuceScorer postDeuceScorer)
+        {
+            postDeuceScorer.listener.aWon();
+        }
+
+        @Override
+        void bScores(PostDeuceScorer postDeuceScorer)
+        {
+            postDeuceScorer.state = deuce;
+        }
+
+
+    };
+
+    public final State advantageB = new State(ADVANTAGE_B);
+
+    class State {
+
+        private String score;
+
+        State(String score){
+            this.score = score;
+        }
+
+        public String score() {
+            return score;
+        }
+
+        void aScores(PostDeuceScorer postDeuceScorer)
+        {
+            if(postDeuceScorer.isAdvantageB()) {
+                postDeuceScorer.state = postDeuceScorer.deuce;
+            } else if(postDeuceScorer.isAdvantageA()) {
+                postDeuceScorer.listener.aWon();
+            } else {
+                postDeuceScorer.state = this;
+            }
+        }
+
+        void bScores(PostDeuceScorer postDeuceScorer)
+        {
+            if (postDeuceScorer.isAdvantageA()) {
+                postDeuceScorer.state = postDeuceScorer.deuce;
+            }
+            else if (postDeuceScorer.isAdvantageB()) {
+                postDeuceScorer.listener.bWon();
+            } else  {
+                postDeuceScorer.state = postDeuceScorer.advantageB;
+            }
+        }
+    }
+
+
+    private State state = deuce;
+
     private GameStateListener listener;
+
+    // Conditionals
+    // Duplication
+    // String comparisons
+    // Too many responsibilities???
 
     @Override
     public String score()
     {
-        return score;
+        return state.score();
     }
 
     @Override
     public void playerAScores()
     {
-        if(score.equals(ADVANTAGE_B))
-            score = DEUCE;
-        else if(score.equals(ADVANTAGE_A))
-            listener.aWon();
-        else
-            score = ADVANTAGE_A;
+        advantageA.aScores(this);
     }
 
     @Override
     public void playerBScores()
     {
-        if(score.equals(ADVANTAGE_A))
-            score = DEUCE;
-        else if (score.equals(ADVANTAGE_B))
-            listener.bWon();
-        else
-            score = ADVANTAGE_B;
+        advantageA.bScores(this);
+    }
+
+    private boolean isAdvantageA()
+    {
+        return state == advantageA;
+    }
+
+    private boolean isAdvantageB()
+    {
+        return state == advantageB;
     }
 
     @Override
